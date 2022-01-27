2e6feed4367fb0e886a50820465c9b4f7b1a3b19
parvesh-25 <75526370+parvesh-25@users.noreply.github.com>
2022-01-27 14:19:52 +0700

    Add files via upload

--- day1.js
+++ day1.js
@@ -109,14 +109,136 @@ console.log(name)
 // nama: nama kalian
 // kelas: x RPL A
 
-function baru(nama,kelas){
-    var name = "Nama: " + nama
-    var klas = "Kelas: " + kelas
-    return function (){
-        console.log(name)
-        console.log(klas)
+// function baru(nama,kelas){
+//     var name = "Nama: " + nama
+//     var klas = "Kelas: " + kelas
+//     return function (){
+//         console.log(name)
+//         console.log(klas)
+//     }
+// }
+
+// var hasil=baru('Altaf', 'X RPL A')
+// hasil()
+
+// Tipe data pada js
+/**
+ * di js ada 7 tipe data
+ * null, undefined,boolean, number, bigInt, string, symbel.
+ * dan tipe data kompleks object
+ * 
+ * loosely typed = yaitu variable tidak menentukan ebuah tipe data, melainkan nilai yang menetukaan tipe data tsb.
+ * 
+ */
+
+/**
+ * tipe data undifinied
+ * Undefined secara umum berarti variabel tersebut sudah dideklarasi, tapi belum didefinisikan (belum diberi nilai).
+ * undifined bisa dijadikan objek cadangan 
+ */
+// let a //
+// function first(params) {
+//     a = "Ini functiom pertama"
+//     console.log(a)
+    
+// }
+
+// function second() {
+//     a = "ini function kedua"
+//     console.log(a)
+// }
+// first()
+// second()
+
+/**
+ * tipe data null
+ * tipe data primitif yang nilainya kosong atau null
+ */
+
+
+/**
+ * number
+ * rentang nilai yang bisa nampung 1  - 2^53. jika sbeuah number memiliki nilai lebih dari rentang itu maka sebaiknya menggunakan tipe data big int
+ */
+
+/** 
+ * NaN atau Mot a Number
+ * nan merupakan nilai numeric yang memberitahu bahwa nilainya it invalid number atau bukan number
+ */
+
+// let a = "hello"
+// if(a >= 17){
+//     a = "anda belum cukup umur"
+//     console.log(a/2)
+// }else{
+//     a = "sudah cukup"
+//     console.log(a)
+// }
+
+/** 
+ * string
+ */
+let a = "fahmi baik"
+console.log(a.length)
+
+let kata = "helloworld"
+for(let i = 0; i< kata.length; i++){
+    console.log(kata[i])
+}
+
+/**
+ * boolean
+ * tipe data yang menemukan kebenaran
+ * dan hanya memiliki 2 nilai (true dan false)
+ */
+
+let belajar = true
+if(belajar){
+    console.log("Kelas ini sedang belajar")
+}
+
+/**
+ * Object
+ * tipe data komplek yang berisi props (properti)
+ */
+
+let identitas = { // yg didalam kurung kurawal adalah properti
+    lastName : "Utsman",
+    email : "dias@gmail.com",
+    instagram : "utssr",
+    linkedin : "diasutsman",
+    phone : "088818181818",
+    addres : {
+        city : "bekasi",
+        number : "17",
+        street : "jalan bahagia"
     }
 }
 
-var hasil=baru('Altaf', 'X RPL A')
-hasil()
+let gtw = {
+    kendaraan: {
+        mobil: "peugeot",
+        motor: "vixion",
+        sepeda: "polygon",
+    },
+    jalan: "Jalan barkah",
+    kota: "bekasi",
+    platNomor: "jabodetabek",
+    kereta: {
+        namaKa: "Argo Bromo anggrek",
+        tujuan: "senen-pasar turi"
+    }
+}
+
+// cara memanggil object
+console.log("nama saya: " + identitas.firstName)
+// cara memanggil yang nested object
+console.log("saya tinggal di : " + identitas.addres.city)
+
+// menggunakan array
+console.log(identitas["firstName"])
+console.log(identitas["addres"] ["street"])
+
+// membuat tabel
+console.table(identitas)
+console.table(gtw)
